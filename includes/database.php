18ca28c1dc28138afc2326bae62c860592da83c8
sebastian-quijas9 <za18011215@zapopan.tecmm.edu.mx>
2024-03-02 17:40:49 -0600

    Actualizacion para subir al server

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,14 @@ if (!extension_loaded('mysqli')) {
     die('La extensión MySQLi no está habilitada.');
 }
 
-$db = mysqli_connect('localhost', 'root', '', 'appsalon');
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME'],
+);
+
+$db->set_charset('utf8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
